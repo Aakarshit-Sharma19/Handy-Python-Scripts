d92049d532d5175b126a524419291e6702a13310
Aakarshit Sharma <asdoon00@gmail.com>
2019-12-08 21:08:21 +0530

    Added remove commas and Seperate By Commas script

---
+++ seperate by commas.py
@@ -0,0 +1,25 @@
+import subprocess
+import sys
+
+file_in = open(sys.argv[1],'r')
+file_out = open(sys.argv[2],'w+')
+
+text_in = file_in.read()
+text_out = ''
+flag = 0
+
+print('Please Wait')
+for x in text_in:
+    if x == ' ':
+        if flag == 0:
+            text_out+=', '
+            flag = 1
+        else:
+            pass
+    else:
+        text_out+=x
+        flag=0
+
+file_out.write(text_out)
+
+print('Done!')
